edecd1c154d31e4b1d112263d7e2bb42fa734a19
si-baba <si-baba@respect-pal.jp>
2023-01-20 14:03:31 +0900

    正規表現の課題修正1回目

--- regularExpressions/index.js
+++ regularExpressions/index.js
@@ -6,12 +6,15 @@ const resultTextChar = document.getElementById("resultTextChar");
 const resultTextAlpInt = document.getElementById("resultTextAlpInt");
 //idが「resultTextNum」のHTML要素を取得している
 const resultTextNum = document.getElementById("resultTextNum");
+//idが「resultSccess」のHTML要素を取得している
+const resultSccess = document.getElementById("resultSccess");
 
 //パスワードの判定用の正規表現をregexAllに代入している
 //半角、数字、記号のみで構成されているかの判断用の正規表現をregexCharに代入している
-const regexChar = /^[a-z\d\-_/*+.,!#$%&()~|]*$/i;
+const regexChar = /^[a-z\d!-~]*$/i;
 //英字数字がどちらも入っているかの判断用の正規表現をregexAlpIntに代入している
-const regexAlpInt = /^(?=.*?[a-z])(?=.*?\d)[a-z\d]*$/i;
+//const regexAlpInt = /^(?=.*?[a-z])(?=.*?\d)[a-z\d]*$/i;
+const regexAlpInt = /[a-z!-~][\d]|[\d][a-z!-~]/i;
 //文字数は適切かどうかの判断用の正規表現をregexNumに代入している
 const regexNum = /^.{8,15}$/;
 
@@ -20,6 +23,8 @@ button.addEventListener("click", function(e) {
 
     //preventDefaultでページが更新されないようにしている
     e.preventDefault();
+
+    var flag = 0;
 
     //入力した内容を取得して、textFormへ代入している
     const textForm = document.getElementById("pwText").value;
@@ -33,23 +38,30 @@ button.addEventListener("click", function(e) {
     resultTextChar.style.display = "none";
     resultTextAlpInt.style.display = "none";
     resultTextNum.style.display = "none";
+    resultSccess.style.display = "none";
 
     //このif文では半角アルファベット、数字、記号以外のものが入力されたらエラー文を表示します
     if(!resultChar){
         //エラー文を表示するためにスタイルを変更している
         resultTextChar.style.display = "block";
+        flag = 1;
     }
 
     //このif文ではパスワードが英字数字をどちらも含んでいるかの判定をして、含めれていなかったらエラー文を表示します
     if(!resultAlpInt){
         //エラー文を表示するためにスタイルを変更している
         resultTextAlpInt.style.display = "block";
+        flag = 1;
     }
 
     //このif文では文字数が適切かを判定し、エラー文を表示します
     if(!resultNum){
         //エラー文を表示するためにスタイルを変更している
         resultTextNum.style.display = "block";
+        flag = 1;
     }
     
+    if(flag == 0){
+        resultSccess.style.display = "block";
+    }
 })
